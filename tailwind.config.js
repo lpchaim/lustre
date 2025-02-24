3f72cad29c1b953642f58becfdfaf44b82c05e55
Lucas-at-592874901619 <desenvolvimento.cheina@gmail.com>
2025-02-24 17:34:53 -0300

    feat: Initial Tailwind CSS support

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{gleam,mjs}"],
+  darkMode: "selector",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
